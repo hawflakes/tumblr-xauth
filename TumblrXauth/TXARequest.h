8a4b546a9e2cafb3d186c9cdedb79c6dbb1741ca
Ehren Kret <ehren@emanata.co>
2013-02-26 13:55:43 -0800

    Add initializers to TXARequest

--- TumblrXauth/TXARequest.h
+++ TumblrXauth/TXARequest.h
@@ -1,4 +1,15 @@
 #import <Foundation/Foundation.h>
 
 @interface TXARequest : NSMutableURLRequest
+
+// Designated Initializer.
+- (id)initWithURL:(NSURL *)url
+              key:(NSString *)key
+           secret:(NSString *)secret
+      accessToken:(NSString *)accessToken
+      tokenSecret:(NSString *)tokenSecret;
+
+// Used for the request to fetch the access token.
+- (id)initWithURL:(NSURL *)url key:(NSString *)key secret:(NSString *)secret;
+
 @end
--- TumblrXauth/TXARequest.m
+++ TumblrXauth/TXARequest.m
@@ -2,12 +2,40 @@
 
 #import <Foundation/Foundation.h>
 
-@interface TXARequest ()
+@interface TXARequest () {
+  NSString *_key;
+  NSString *_secret;
+  NSString *_accessToken;
+  NSString *_tokenSecret;
+}
 + (NSString *)generateNonce;
 + (NSString *)generateTimestamp;
 @end
 
 @implementation TXARequest
+
+- (id)initWithURL:(NSURL *)url
+              key:(NSString *)key
+           secret:(NSString *)secret
+      accessToken:(NSString *)accessToken
+      tokenSecret:(NSString *)tokenSecret {
+  self = [super initWithURL:url];
+  if (self) {
+    _key = [key copy];
+    _secret = [secret copy];
+    _accessToken = [accessToken copy];
+    _tokenSecret = [tokenSecret copy];
+  }
+  return self;
+}
+
+- (id)initWithURL:(NSURL *)url key:(NSString *)key secret:(NSString *)secret {
+  return [self initWithURL:url
+                       key:key
+                    secret:secret
+               accessToken:nil
+               tokenSecret:nil];
+}
 
 #pragma mark - Class Extension Methods
 
